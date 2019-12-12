870324ab6d9d69f91f598f940b870cca42b501bb
Denis Ivanov <ltybc3528@mail.ru>
2019-12-13 02:51:40 +0300

    Зеленый тест: создание авл дерева

---
+++ TestingAVLTree/AVLTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingAVLTree
+{
+    class AVLTree<T> : IComparable<T> where T : IComparable
+    {
+        public AVLTree(T value, AVLTree<T> parent)
+        {
+        }
+
+        public int CompareTo(T other)
+        {
+            return 5;
+        }
+    }
+}
---
+++ TestingAVLTree/TestAVLTree.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingAVLTree
+{
+    [TestFixture]
+    class TestAVLTree
+    {
+        [Test]
+        public void CreateAVLTreeGivenItsValueAndParent()
+        {
+            AVLTree<int> tree = new AVLTree<int>(10, null); 
+        }
+    }
+}
